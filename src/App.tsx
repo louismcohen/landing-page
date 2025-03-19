d10254b37ddd6f7c5fe3c5b2a649bfab1eeb447a
Louis Cohen <louis@skymob.net>
2025-03-19 15:22:28 -0700

    lots of changes, glass look

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,128 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  PaperPlaneTilt,
+  InstagramLogo,
+  Globe,
+  Phone,
+} from '@phosphor-icons/react';
+import type { IconProps } from '@phosphor-icons/react';
+import PhoneChatIcon from './assets/phone-chat.svg?react';
+// import logoLightBg from './assets/logo-light-bg.svg';
+import logoDarkBg from './assets/logo-dark-bg.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const PhoneChat = ({ size = 48, color = 'currentColor' }: IconProps) => {
+  return <PhoneChatIcon height={size} width={size} fill={color} />;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const iconMap = {
+  mail: PaperPlaneTilt,
+  instagram: InstagramLogo,
+  website: Globe,
+  phone: Phone,
+  phoneChat: PhoneChat,
+};
+
+interface TileProps {
+  title: string;
+  subtitle?: string;
+  icon: keyof typeof iconMap;
+  href?: string;
 }
 
-export default App
+const Tile = ({ title, subtitle, icon, href }: TileProps) => {
+  const IconComponent = iconMap[icon];
+
+  return (
+    <a href={href} target='_blank' rel='noreferrer'>
+      <div className='flex flex-row gap-4 w-full justify-start items-center p-4 text-light/90 bg-neutral-100/25 border border-neutral-50/20 hover:bg-yellow-500/20 hover:border-yellow-500/33 active:bg-yellow-500/30 active:border-yellow-500/50 hover:scale-102 active:scale-98 transition-all cursor-pointer rounded-xl shadow-lg hover:shadow-xl active:shadow-md'>
+        <IconComponent weight='regular' size='36' />
+        <div className='flex flex-col gap-0 justify-center items-start'>
+          <p className='font-semibold text-lg select-none'>{title}</p>
+          <p className='font-regular text-sm'>{subtitle}</p>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+interface SectionProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const Section = ({ title, children }: SectionProps) => {
+  return (
+    <div className='flex flex-col gap-2 w-full'>
+      <p className='font-extrabold uppercase tracking-tight text-xl text-light text-left font-[Gabarito] drop-shadow-md'>
+        {title}
+      </p>
+      {children}
+    </div>
+  );
+};
+
+const App = () => {
+  const [randomImage, setRandomImage] = useState<string>('');
+
+  useEffect(() => {
+    const importImages = async () => {
+      const imageImports = import.meta.glob(
+        './assets/images/*.{png,jpg,jpeg,svg}'
+      );
+      const imagePaths = Object.keys(imageImports).map((path) =>
+        path.replace('./assets', '/src/assets')
+      );
+
+      const randomIndex = Math.floor(Math.random() * imagePaths.length);
+      setRandomImage(imagePaths[randomIndex]);
+    };
+
+    importImages();
+  }, []);
+
+  return (
+    <div
+      className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-cover bg-center'
+      style={{ backgroundImage: `url(${randomImage})` }}
+    >
+      <div className='fixed w-full h-full bg-dark/90 flex justify-center items-center p-4'>
+        <div className='flex flex-col w-fit justify-center items-center gap-2 overflow-hidden overscroll-none'>
+          <img src={logoDarkBg} alt='logo' width={400} />
+          <div className='flex flex-col gap-8 bg-neutral-50/20 px-4 py-6 md:px-6 md:py-8 border border-neutral-50/20 rounded-2xl drop-shadow-2xl shadow-xl w-full backdrop-blur-sm'>
+            <Section title='Learn More'>
+              <Tile
+                title='Website'
+                subtitle='confidentialdetailers.com'
+                icon='website'
+                href='https://www.confidentialdetailers.com'
+              />
+              <Tile
+                title='Instagram'
+                subtitle='@confidentialdetailers'
+                icon='instagram'
+                href='https://www.instagram.com/confidentialdetailers'
+              />
+            </Section>
+            <Section title='Contact Us'>
+              <Tile
+                title='Call or Text'
+                subtitle='(310) 993-5094'
+                icon='phoneChat'
+                href='tel:+13109935094'
+              />
+              <Tile
+                title='Email'
+                subtitle='rg@confidentialdetailers.com'
+                icon='mail'
+                href='mailto:rg@confidentialdetailers.com'
+              />
+            </Section>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
